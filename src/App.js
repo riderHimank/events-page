d1e51be3fb680b730fb9e28824a1c4ba754a703f
riderHimank <himankb500@gmail.com>
2023-12-22 19:08:17 +0530

    Shader loaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { OrbitControls, Stats, Html, Text } from "@react-three/drei";
+import Background from "./Background";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas camera={{ position: [0, 0, 2], fov: 50 }}>
+        <OrbitControls />
+        <Background />
+      </Canvas>
+    </>
   );
-}
+};
 
 export default App;
